d651a51936e116ab4ddb3dac6193829acdeae3be
Infinity0901 <147404427+Infinity0901@users.noreply.github.com>
2024-11-18 13:47:28 +0100

    main

--- src/script.js
+++
@@ -1,6 +0,0 @@
-// Script pour le formulaire de contact
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Merci pour votre message ! Je reviendrai vers vous rapidement.');
-    this.reset();
-});
